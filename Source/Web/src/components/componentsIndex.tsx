13400eacc26f99d461e60f955a95ee81b5e0a426
Grzegorz00 <g.swiecickii@gmail.com>
2024-03-20 20:09:14 +0100

    added componentsIndex for easy imports

---
+++ Source/Web/src/components/componentsIndex.tsx
@@ -0,0 +1,11 @@
+import TextInput from "./TextInput/TextInput"
+import LoadingCircle from "./LoadingCircle/LoadingCircle"
+import CustomButton from "./CustomButton/Button";
+import FormTextInput from "./Formik/FormTextInput/FormTextInput"
+
+export {
+    TextInput,
+    LoadingCircle,
+    CustomButton,
+    FormTextInput
+};
