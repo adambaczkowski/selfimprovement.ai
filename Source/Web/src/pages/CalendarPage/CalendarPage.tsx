51ed072969b7d1250fb59ed2f81a2b8bb04733fd
Grzegorz00 <g.swiecickii@gmail.com>
2024-05-06 18:12:14 +0200

    added new calendar page

---
+++ Source/Web/src/pages/CalendarPage/CalendarPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './CalendarPage.module.scss';
+
+type Props = {};
+
+function CalendarPage({}: Props) {
+
+  return (
+    <div className={styles.background_container}>
+      <h1>Calendar Page</h1>
+    </div>
+  );
+}
+
+export default CalendarPage;
