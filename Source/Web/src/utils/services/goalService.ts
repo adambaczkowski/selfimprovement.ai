df7e9d6571601fdf9e0470ae541e900441a0a846
Mati <mlodochowski.mateusz@gmail.com>
2024-05-17 12:27:50 +0200

    work

--- Source/Web/src/utils/services/goalService.ts
+++ Source/Web/src/utils/services/goalService.ts
@@ -1,10 +1,7 @@
 import { CreateGoalCommand, GoalService } from "../api/goal"
 
 export const createGoal = async (command : CreateGoalCommand) => {
-    const userToken = JSON.parse(localStorage.getItem("userToken") || '""');
-    console.log('#usertkn', userToken);
     const response = await GoalService.post({requestBody: command});
-    console.log('#response', response);
     return response;
 }
 
