0a70a9ef37ef2740fb0f0dcdf43b58999e2e6c16
Mati <mlodochowski.mateusz@gmail.com>
2024-05-18 19:36:31 +0200

    fix

--- Source/GoalApi/Goal/Dtos/GoalDetailsDto.cs
+++ Source/GoalApi/Goal/Dtos/GoalDetailsDto.cs
@@ -4,6 +4,7 @@ namespace GoalApi.Goal.Dtos;
 
 public class GoalDetailsDto
 {
+    public Guid Id { get; init; }
     public GoalCategories Category { get; init; }
     public TimeAvailability TimeAvailability { get; init; }
     public DateTime StartDate { get; init; }
