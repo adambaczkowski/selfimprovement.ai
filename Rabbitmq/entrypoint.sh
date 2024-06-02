5eb1c91ce93c3df345d6181ed8ac6f3ebebffaef
Adam Bączkowski <adambaczkowski@icloud.com>
2024-06-02 19:30:10 +0200

    erlang cookie testing

--- Rabbitmq/entrypoint.sh
+++ Rabbitmq/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+openssl rand -base64 32 | tr -d '\n' | cut -c1-64 > /var/lib/rabbitmq/.erlang.cookie
 chmod 600 /var/lib/rabbitmq/.erlang.cookie
 exec docker-entrypoint.sh rabbitmq-server
