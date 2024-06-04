2caf8e3b513aec6c6046ec63f7a33ef8b5a01f11
Grzegorz00 <g.swiecickii@gmail.com>
2024-06-04 22:32:32 +0200

    updated profile edition

---
+++ Source/Web/src/utils/helpers/stringToBlob.tsx
@@ -0,0 +1,9 @@
+export function base64ToBlob(base64: string, contentType: string): Blob {
+  const byteCharacters = atob(base64);
+  const byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+  return new Blob([byteArray], { type: contentType });
+}
